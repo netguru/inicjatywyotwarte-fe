c0c2033f23087cf7720782c8fa18caf3d0d4d867
Bartosz Sobczynski <bartosz.sobczynski@netguru.pl>
2020-07-08 10:31:35 +0200

    Add box shadow to  thumbnail

--- src/pages/OneResourcePage/OneResourcePage.js
+++ src/pages/OneResourcePage/OneResourcePage.js
@@ -103,6 +103,7 @@ const ResourceIcon = styled.div`
     max-width: 100%;
     max-height: 100%;
     border-radius: ${props => props.theme.borderRadiusMedium};
+    box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.1);
   }
 `
 
