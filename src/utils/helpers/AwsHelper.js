faf858652a57461cf76eb0bb92c7b57c39d93d2a
Bartosz Sobczynski <bartosz.sobczynski@netguru.pl>
2020-07-09 12:57:49 +0200

    Fix env var names

--- src/utils/helpers/AwsHelper.js
+++ src/utils/helpers/AwsHelper.js
@@ -2,7 +2,7 @@ import AWS from 'aws-sdk'
 import { signedUrlExpireSeconds } from 'constants/constants';
 
 const useLocal = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
-const bucketUrl = process.env.STORAGE_BUCKET_URL
+const bucketUrl = process.env.REACT_APP_STORAGE_BUCKET_URL
 
 const s3bucket = new AWS.S3({
   accessKeyId: 'fakeLocalstackAccessKey',
